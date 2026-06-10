733ef6b65b73d338717c2517473d1177084d9616
LeeFoS6092 <LeeFoS6092@users.noreply.github.com>
2026-06-10 13:45:56 +0000

    deploy: a05b605b849851c1c67520751d5f8774158f242d

---
+++ pr-preview/pr-3/js/main.js
@@ -0,0 +1,75 @@
+// js/main.js
+/* global Swiper */
+
+const title = document.querySelector('.features__title')
+
+new Swiper('.features-slider', {
+  centeredSlides: true,
+  slidesPerView: 1.3,
+  loop: true,
+  speed: 500,
+  grabCursor: true,
+  spaceBetween: 5,
+
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false
+  },
+
+  breakpoints: {
+    768: {
+      slidesPerView: 2
+    }
+  },
+
+  on: {
+    init(swiper) {
+      const active =
+        swiper.slides[swiper.activeIndex]
+
+      title.textContent =
+        active.dataset.title
+
+      playActiveVideo(swiper)
+    },
+
+    slideChange(swiper) {
+      const active =
+        swiper.slides[swiper.activeIndex]
+
+        changeContent(title, active.dataset.title)
+
+      playActiveVideo(swiper)
+    }
+  }
+})
+
+
+function playActiveVideo(swiper) {
+
+  document
+    .querySelectorAll('.slide-card video')
+    .forEach(video => {
+
+      video.pause()
+      video.currentTime = 0
+
+    })
+
+  const activeVideo =
+    swiper.slides[swiper.activeIndex]
+      .querySelector('video')
+
+  if (activeVideo) {
+    activeVideo.play()
+  }
+}
+
+
+function changeContent(obj, text) {
+    obj.style.opacity = 0;
+    setTimeout(() => {
+        obj.innerHTML = text
+        obj.style.opacity = 1;
+    }, 150)
+}
